f1d9508a65ad4d9bc5ca00f3f782b65ce3507f06
Dairo Mosquera <dairoandres115@outlook.com>
2025-03-20 12:29:02 -0500

    Update logger.py

--- python/logger.py
+++ python/logger.py
@@ -1,51 +1,65 @@
 """Logger configuration to use ``colorlog.ColoredFormatter``
 as the default formatter.
 
-Requires: Python 3.6+
-
-Install: ``pip install colorlog``
-
 Visit https://pypi.org/project/colorlog/ for more information.
 """
 
 import logging
+import os
+import socket
 from logging.config import dictConfig
 
-FMT = "%(log_color)s%(asctime)s | %(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
-DATE_FMT = "%Y-%m-%d %H:%M:%S"
+STANDARD_FMT = '%(asctime)s | %(levelname)-8s | %(message)s'
+COLORED_FMT = '%(log_color)s%(asctime)s%(reset)s | %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s'
+DATEFMT = '%Y-%m-%d %H:%M:%S'
 
-dictConfig(
-    {
-        'version': 1,
-        'formatters': {
-            'colored': {
-                '()': 'colorlog.ColoredFormatter',
-                'format': FMT,
-                'datefmt': DATE_FMT,
-            }
-        },
-        'handlers': {
-            'console': {
-                '()': 'logging.StreamHandler',
-                'formatter': 'colored',
+DOCKER_LOGS_FOLDER = '/usr/logs'
+LOCAL_LOGS_FOLDER = '.logs'
+
+
+def set_logger_conf(on_docker: bool = False):
+    path = DOCKER_LOGS_FOLDER if on_docker else LOCAL_LOGS_FOLDER
+    file = f'{socket.gethostname()}.log' if on_docker else 'info.log'
+    filepath = os.path.join(path, file)
+
+    dictConfig(
+        {
+            'version': 1,
+            'formatters': {
+                'standard': {
+                    '()': 'logging.Formatter',
+                    'fmt': STANDARD_FMT,
+                    'datefmt': DATEFMT,
+                },
+                'colored': {
+                    '()': 'colorlog.ColoredFormatter',
+                    'format': COLORED_FMT,
+                    'datefmt': DATEFMT,
+                },
+            },
+            'handlers': {
+                'console': {
+                    '()': 'logging.StreamHandler',
+                    'formatter': 'colored',
+                    'level': logging.DEBUG,
+                },
+                'file': {
+                    'level': 'INFO',
+                    'formatter': 'standard',
+                    'class': 'logging.handlers.RotatingFileHandler',
+                    'filename': filepath,
+                    'maxBytes': 104857600,
+                    'backupCount': 4,
+                    'encoding': 'utf8',
+                },
+            },
+            'root': {
+                'handlers': ['console', 'file'],
                 'level': logging.DEBUG,
             },
-            # 'file': {
-            #     '()': 'logging.handlers.RotatingFileHandler',
-            #     'filename': 'log.log',
-            #     'formatter': 'colored',
-            #     'level': logging.DEBUG,
-            #     'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            #     'backupCount': 2,
-            #     'encoding': 'utf-8',
-            # },
-        },
-        'root': {
-            'handlers': ['console'],
-            'level': logging.DEBUG,
-        },
-    }
-)
+        }
+    )
+
 
 logger = logging.getLogger()
 """Colored logger instance."""
