884f9f4542956a38a70c0588132646447d539238
Dairo Mosquera <dairoandres115@outlook.com>
2025-05-29 10:54:24 -0500

    Create graceful_killer.py

---
+++ python/graceful_killer.py
@@ -0,0 +1,17 @@
+import signal
+
+
+class GracefulKiller:
+    kill_now = False
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(GracefulKiller, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, sig, frame):
+        self.kill_now = True
